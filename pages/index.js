faf88a91128969639b43939ed3e2f43f2e2c280c
nenadmne <cosovicnenad14@gmail.com>
2023-11-27 17:24:35 +0100

    Using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-function HomePage(props) {
+function HomePage({ products }) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
     </ul>
   );
 }
 
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: "p1", name: "Product 1" },
+        { id: "p2", name: "Product 2" },
+        { id: "p3", name: "Product 3" },
+      ],
+    },
+  };
+}
 export default HomePage;
