e0a7ebba711c24fbaf89deeb46133527e8088e7e
nenadmne <cosovicnenad14@gmail.com>
2023-11-28 22:53:21 +0100

    using redirect if there is no data

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,30 @@ function HomePage({ products }) {
 }
 
 export async function getStaticProps() {
-  //fs and path usage for dummy backend data
+  // fs and path usage for dummy backend data
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // example of using redirect if there is no data at all
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  // if data is empty generating 404-not found
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
-    //regenerating page every 10 seconds
+    // regenerating page every 10 seconds
     revalidate: 10,
   };
 }
