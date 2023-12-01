013e24f6e1bfe7432faae895d164ae071d0de006
nenadmne <cosovicnenad14@gmail.com>
2023-12-01 20:06:52 +0100

    Dynamically storing paths

--- pages/[pid].js
+++ pages/[pid].js
@@ -3,6 +3,10 @@ import fs from "fs/promises";
 import path from "path";
 
 function ProductDetailPage({ product }) {
+  // if fallback is set to fallback:true, this is important to add since page is not pre-rendered but renders just in time after request
+  if (!product) {
+    return <p> Loading... </p>;
+  }
   return (
     <Fragment>
       <h1> {product.title} </h1>
@@ -11,15 +15,18 @@ function ProductDetailPage({ product }) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export async function getStaticProps(context) {
   // getting dynamic parametars trough destructuring context
   const { params } = context;
   const productId = params.pid;
-
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-
+  const data = await getData();
   const product = data.products.find((item) => item.id === productId);
 
   return {
@@ -30,26 +37,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((item) => item.id);
+  const params = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      {
-        params: {
-          pid: "p1",
-        },
-      },
-      {
-        params: {
-          pid: "p2",
-        },
-      },
-      {
-        params: {
-          pid: "p3",
-        },
-      },
-    ],
+    // paths should be array of objects where every object contains object with params and its value
+    paths: params,
     fallback: false,
   };
 }
-
 export default ProductDetailPage;
