2c22f45de811689f35850a4d70652572d0dc7d0d
nenadmne <cosovicnenad14@gmail.com>
2023-12-01 20:46:45 +0100

    For getServerSideProps we dont have to define paths

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -3,7 +3,9 @@ function UserProfile({ username }) {
 }
 
 // We use getServerSideProps when we want to access request object like header or cookies. Usefull for auth pages
+// For getServerSideProps no need to define paths for running code
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
   return {
     props: {
       username: "Max",
