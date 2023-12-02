6f8e641a62fbbf615f33c21d33b2f6aae6459b61
nenadmne <cosovicnenad14@gmail.com>
2023-12-02 15:54:27 +0100

    Data fetching trought getStaticProps

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,41 +1,38 @@
-import { useEffect, useState } from "react";
-
-function LastSales() {
-  const [sales, setSales] = useState();
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      "https://next-js-course-c7fc9-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedData = [];
-
-        for (const key in data) {
-          transformedData.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-
-        setSales(transformedData);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading || !sales) {
+function LastSales(props) {
+  if (!props.sales) {
     return <p> Loading...</p>;
   }
 
   return (
     <ul>
-      {sales.map((item) => (
+      {props.sales.map((item) => (
         <li key={item.id}>{item.username}</li>
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return fetch(
+    "https://next-js-course-c7fc9-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const transformedData = [];
+
+      for (const key in data) {
+        transformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      return {
+        props: { sales: transformedData },
+        revalidate: 10,
+      };
+    });
 }
 
 export default LastSales;
